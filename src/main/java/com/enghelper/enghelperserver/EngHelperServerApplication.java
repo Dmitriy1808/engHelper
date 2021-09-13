befbe00c19f5de76c2154db885af7451e50096d4
Dmitry Golcev <dmitry.golcev@gmail.com>
2021-09-13 21:33:14 +0700

    Initial commit

---
+++ src/main/java/com/enghelper/enghelperserver/EngHelperServerApplication.java
@@ -0,0 +1,13 @@
+package com.enghelper.enghelperserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngHelperServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngHelperServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enghelper/enghelperserver/EngHelperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enghelper.enghelperserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngHelperServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
